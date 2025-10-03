32d1324c74111ed8e308e83bdc9fedd414bc36f0
hariharans01 <harigokuljr11@gmail.com>
2025-10-03 13:55:51 +0530

    hari| update login page

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmnj2KAwjADtLWqO1Rr_IbWvhvmwCQHJ4",
+  authDomain: "react-blog-01022002.firebaseapp.com",
+  projectId: "react-blog-01022002",
+  storageBucket: "react-blog-01022002.firebasestorage.appspot.com",
+  messagingSenderId: "870857838497",
+  appId: "1:870857838497:web:1deefe522f4dcbf05705e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth (app) ;
+export const provider = new GoogleAuthProvider();
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,19 @@
+import  { auth, provider} from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+
 export const Login = () => {
-    return <div>Login Page</div>
-} 
+    
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth,provider)
+        console.log(result);
+    };
+    
+    return (
+        <div>
+            <p> SIGN IN TO CONTINUE !! </p>
+            <button onClick={signInWithGoogle}> Sign in with Google </button>
+        </div>
+
+    );
+    
+};
